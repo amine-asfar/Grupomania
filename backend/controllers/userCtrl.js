65b0ef97d244fd4c9cf8c8b863423c2a80a55fbe
Amine Asfar <amine.asfar294@gmail.com>
2022-05-20 23:03:52 +0200

    signup function

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -4,47 +4,59 @@ const bcrypt = require('bcrypt');
 var asyncLib = require('async');
 
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
+    // Params
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var bio = req.body.bio;
+
     asyncLib.waterfall([
         function (done) {
-            models.User.findone({
+            models.User.findOne({
                 attributes: ['email'],
-                where: { email: req.body.email }
+                where: { email: email }
             })
-                .then(function (userfound) {
-                    done(null, userfound);
+                .then(function (userFound) {
+                    done(null, userFound);
                 })
                 .catch(function (err) {
-                    return res.status(500).json({ message: "L'email utilisé correspond déja a un compte existant" })
-                })
-        }, function (userfound, done) {
-            if (!userfound) {
-                bcrypt.hash(req.body.password, 10, function (err, bcryptPassword) {
-                    done(null, userfound, bcryptPassword);
+                    return res.status(500).json({ 'error': 'unable to verify user' });
+                });
+        },
+        function (userFound, done) {
+            if (!userFound) {
+                bcrypt.hash(password, 5, function (err, bcryptedPassword) {
+                    done(null, userFound, bcryptedPassword);
                 });
             } else {
-                return res.status(409).json({ 'error': 'user already exist' })
+                return res.status(409).json({ 'error': 'user already exist' });
             }
-        }, function (bcryptPassword, done) {
-            let newUser = models.User.create({
-                email: req.body.email,
-                password: bcryptPassword
+        },
+        function (userFound, bcryptedPassword, done) {
+            if (!userFound) { }
+            var newUser = models.User.create({
+                email: email,
+                username: username,
+                password: bcryptedPassword,
+                bio: bio,
+                isAdmin: 0
             })
                 .then(function (newUser) {
-                    done(newUser)
+                    done(newUser);
                 })
                 .catch(function (err) {
                     return res.status(500).json({ 'error': 'cannot add user' });
                 });
         }
-
     ], function (newUser) {
         if (newUser) {
             return res.status(201).json({
                 'userId': newUser.id
             });
         } else {
-            return res.status(500).json({ 'error': 'cannont add user' })
+            return res.status(500).json({ 'error': 'cannot add user' });
         }
-    })
+    });
 };
+
